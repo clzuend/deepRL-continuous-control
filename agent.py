0e73d7f7cd497ca7375c00f370f8035a5617451e
Christian L. Zuend <clzuend@gmail.com>
2020-05-07 07:33:21 +0200

    solved the 20 agent environment

--- agent.py
+++ agent.py
@@ -11,8 +11,8 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torchviz import make_dot
 
-BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 1024       # minibatch size
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 128       # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
